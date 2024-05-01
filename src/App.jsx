9d860fedd9357be023ddc5774e7c76228b2a2267
GenaNorman <normankamando@gmail.com>
2024-04-30 19:48:24 -0700

    Create react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+const [ready, setIsReady] = useState('');
+const x = 20;
+const x = 34;
+
+return <div>
+  worldwise
+</div>
+}
+export default App;
